b372316b3d5e0678cae9f3b83f2bfbe2127bf7cf
t-kusakabe <ttmac0704@gmail.com>
2016-05-30 12:15:12 +0900

    add_tutorial1_js_file

---
+++ tutorial1.js
@@ -0,0 +1,16 @@
+// tutorial1.js
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className='commentBox'>
+        Hello, World! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
